6cb60d9d7acac39759606160564293c315c4d49a
Karthik R <karthik@nedholdings.com>
2023-03-23 05:57:16 +0000

    New file commited

---
+++ index.php
@@ -0,0 +1,16 @@
+    <?php
+    $wall_paper = "ooty.jpeg";
+    ?>
+    <html>
+    <head>
+    <style type="text/css">
+    body {
+    background-image: url('<?php echo $wall_paper;?>');
+    }
+    </style>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Welcome To Nilgiris</title>
+    </head>
+    <body>
+    </body>
+    </html>
